0f10490b08610d646e05bd5b643deafe7e5ace84
Qianchuan Ye <jonathye@gmail.com>
2017-06-20 00:52:22 +0800

    Handle some corner cases.

--- functions/push_line.fish
+++ functions/push_line.fish
@@ -1,4 +1,9 @@
 function push_line -d "Push current buffer onto the buffer stack and clear current buffer"
+    set -l __line (commandline)
+    if test -z "$__line"
+        return 0
+    end
+
     if not count $__push_line_stack >/dev/null
         function __pop_line -e fish_postexec
             pop_line
@@ -8,6 +13,6 @@ function push_line -d "Push current buffer onto the buffer stack and clear curre
         end
     end
 
-    set -g __push_line_stack (commandline -C):(commandline) $__push_line_stack
+    set -g __push_line_stack (commandline -C):"$__line" $__push_line_stack
     commandline ""
 end
